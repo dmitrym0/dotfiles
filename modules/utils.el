8e380938a0219d4843f87ef112a53a525582b0d9
Dmitry M <dmitrym@gmail.com>
2024-11-19 10:30:18 -0800

    .

---
+++ modules/utils.el
@@ -0,0 +1,19 @@
+(defun reload-init-file ()
+  "Reload .emacs"
+  (interactive)
+  (load-file user-init-file))
+
+(global-set-key (kbd "C-c C-l") 'reload-init-file)
+
+;; based on http://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (concat "Do you really want to delete file " filename " ?"))
+            (progn
+              (delete-file filename)
+              (message "Deleted file %s." filename)
+              (kill-buffer)))
+      (message "Not a file visiting buffer!"))))
